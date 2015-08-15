4a6740a9a3f9982ab0f5646f4ff31fd14da2ae20
Sean <sean@hydreon.com>
2015-08-15 10:26:20 -0400

    Update Main.php

--- src/Sean_M/FootBlock/Main.php
+++ src/Sean_M/FootBlock/Main.php
@@ -9,6 +9,8 @@ use pocketmine\math\Vector3;
 use pocketmine\block\Block;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 use FootBlock\Commands\Block;
 
@@ -29,6 +31,41 @@ class Main extends PluginBase implements Listener{
         $level = $player->getLevel();
         $block = Block::get($player->getInventory()->getItemInHand()->getId(), 0);  
         $floor = $player->getFloorX(), $player->getFloorY() - 1, $player->getFloor();
-        $level->setBlock(new Vector3($floor, $block));
+        if($this->isPlayer($sender){
+            $level->setBlock(new Vector3($floor, $block));
+        }
      }
+
+    public function onCommand(CommandSender $sender, Command $cmd, $label,array $args){
+        if(strtolower($cmd->getName()) === "FootBlock"){
+            if($sender instanceof Player){
+                if($this->isPlayer($sender)){
+                    $this->removePlayer($sender);
+                    $sender->sendMessage(TextFormat::GOLD . "You have disabled FootBlock.");
+                    return true;
+                }
+                else{
+                    $this->addPlayer($sender);
+                    $sender->sendMessage(TextFormat::GREEN . "You have enabled FootBlock.");
+                    return true;
+                }
+            }
+            else{
+                $sender->sendMessage(TextFormat::RED . "Please use this command in-game.");
+                return true;
+            }
+        }
+    }
+
+    public function addPlayer(Player $player){
+        $this->players[$player->getName()] = $player->getName();
+    }
+
+    public function isPlayer(Player $player){
+        return in_array($this->players[$player->getName());
+    }
+
+    public function removePlayer(Player $player){
+        unset($this->players[$player->getName()]);
+    }
 }
