33c0974752b7638de9bf81d5a6257078c94aa161
Sean <sean@hydreon.com>
2015-08-15 07:09:27 -0400

    Update Main.php

--- src/Sean_M/FootBlock/Main.php
+++ src/Sean_M/FootBlock/Main.php
@@ -1,1 +1,33 @@
+<?php
+namespace Sean_M\FootBlock;
 
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\math\Vector3;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\Player;
+
+class Main extends PluginBase implements Listener{
+
+    public $config;
+
+     public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+          $this->saveDefaultConfig();
+          $this->reloadConfig();
+          $config = $this->getConfig()->getAll();
+            $this->getLogger()->info(TextFormat::GREEN . "FootBlock by Sean_M enabled!");
+     }
+     public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED . "DeathDamage by Sean_M disabled!");
+     }
+   
+     public function onPlayerMove(PlayerMoveEvent $event){
+        $player = $event->getPlayer();
+        $level = $player->getLevel();
+        $block = $config["block"];
+          $player->$level->setBlock(new Vector3($p->getFloorX(), $p->getFloorY(), $p->getFloorZ()), $block);
+     }
+}
