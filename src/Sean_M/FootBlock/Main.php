9187c0af0ea46a467ef05ba7c51bdf2173dff111
Sean <sean@hydreon.com>
2015-08-15 09:31:42 -0400

    Update Main.php

--- src/Sean_M/FootBlock/Main.php
+++ src/Sean_M/FootBlock/Main.php
@@ -6,28 +6,26 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
 use pocketmine\math\Vector3;
+use pocketmine\block\Block;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\Player;
 
 class Main extends PluginBase implements Listener{
 
-    public $config;
-
      public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-          $this->saveDefaultConfig();
-          $this->reloadConfig();
-          $config = $this->getConfig()->getAll();
-            $this->getLogger()->info(TextFormat::GREEN . "FootBlock by Sean_M enabled!");
+        $this->getLogger()->info(TextFormat::GREEN . "FootBlock by Sean_M enabled!");
      }
+
      public function onDisable(){
-        $this->getLogger()->info(TextFormat::RED . "DeathDamage by Sean_M disabled!");
+        $this->getLogger()->info(TextFormat::RED . "FootBlock by Sean_M disabled!");
      }
    
      public function onPlayerMove(PlayerMoveEvent $event){
         $player = $event->getPlayer();
         $level = $player->getLevel();
-        $block = $config["block"];
-          $player->$level->setBlock(new Vector3($p->getFloorX(), $p->getFloorY(), $p->getFloorZ()), $block);
+        $block = Block::get($player->getInventory()->getItemInHand()->getId(), 0);  
+        $floor = $player->getFloorX(), $player->getFloorY() - 1, $player->getFloor();
+        $level->setBlock(new Vector3($floor, $block));
      }
 }
